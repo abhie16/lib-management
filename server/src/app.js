15b8e9a5b73d5ce55a06bd67b192e872199403b7
Abhishek pandey <abhie9336@gmail.com>
2023-05-16 18:20:43 +0530

    serving frontend static files to the backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors')
 
 const { getBooks ,postBook, getBook, deleteBook} = require('../src/controllers/books.controller');
@@ -10,6 +11,8 @@ app.use(cors({
 }))
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 
 app.get('/books', getBooks);
 app.post('/books', postBook);
